704d75f959b51d0027830f3e945a9efa6d7e0cb4
akani28 <nietocevallos61@gmail.com>
2024-01-21 03:40:13 -0500

    DOM

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,24 +4,25 @@
 
 /* Step 2 - Variables */
 let fullname = 'Angel Nieto'
-let currentYear
+let currentYear = '2024'
 let profilePicture = 'images/foto1.jpg'
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name')
 const foodElement = document.getElementById('food')
 const yearElement = document.querySelector('#year')
-const imageElement = docuement.getElementById('image')
+const imageElement = document.getElementById('image')
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = (`<strong>${fullname}</strong>`)
-currentYear = document.textContent("2024")
+yearElement.textContent = currentYear
 imageElement.setAttribute('src', profilePicture)
 imageElement.setAttribute('alt', `Profile image of ${fullname}`)
 
 
 /* Step 5 - Array */
-
+const foods =["encebollado", "arroz con menestra", "empanadas", "hamburguer"]
+foods.forEach((food)=>(foodElement.innerHTML = (`${food}`)))
 
 
 
