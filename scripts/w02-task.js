0c3727bc0184b24ec7b860458bb981329259a94e
akani28 <nietocevallos61@gmail.com>
2024-01-21 02:35:01 -0500

    update files

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,21 +3,21 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
-
+let fullname = 'Angel Nieto'
+let currentYear
+let profilePicture = 'images/foto1.jpg'
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById('name')
+const foodElement = document.getElementById('food')
+const yearElement = document.querySelector('#year')
+const imageElement = docuement.getElementById('image')
 
 /* Step 4 - Adding Content */
-
-
-
-
+nameElement.innerHTML = (`<strong>${fullname}</strong>`)
+currentYear = document.textContent("2024")
+imageElement.setAttribute('src', profilePicture)
+imageElement.setAttribute('alt', `Profile image of ${fullname}`)
 
 
 /* Step 5 - Array */
